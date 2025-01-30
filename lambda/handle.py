28ae4c25af81a43a2d108c7f55ea259b768705ff
ander <andersonbarreto2001@gmail.com>
2025-01-31 00:53:08 +0100

    fix endpoint for localstack with docker, the endpoint should be http://host.docker.internal:4566

--- lambda/handle.py
+++ lambda/handle.py
@@ -4,31 +4,27 @@ import boto3
 
 def lambda_handler(event, context):
     try:
-        # parser body request
+        print("Received event:", json.dumps(event))
+
         body = json.loads(event["body"])
         id = body.get("UserId")
         data = body.get("data")
 
-        if not id or not data:
-            return {
-                "statusCode": 400,
-                "body": json.dumps({"error": "Missing 'id' or 'data' in request"}),
-            }
-        # insert data on dynamodb
         dynamodb = boto3.resource(
             "dynamodb",
             region_name="us-east-1",
-            endpoint_url="http://localhost:4566",  # LocalStack URL
+            endpoint_url="http://host.docker.internal:4566",  # LocalStack URL
         )
+
         table = dynamodb.Table("TestTable")
-        table.put_item(Item={"UserId": id, "data": data})
-        
         response = table.put_item(Item={"UserId": id, "data": data})
         print(f"DynamoDB Response: {response}")
-        
+
         return {
-            "status_code": 200,
-            "body": json.dumps({"status": "Data inserted succesfully"}),
+            "statusCode": 200,
+            "body": json.dumps({"status": "Data inserted successfully"}),
         }
+
     except Exception as err:
+        print("Error:", str(err))
         return {"statusCode": 500, "body": json.dumps({"error": str(err)})}
